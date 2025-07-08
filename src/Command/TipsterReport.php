e97098f4af95dc7fd40dc7dc453f638516d98b95
jordi <jordi.donadeu@nl.team.blue>
2025-07-08 11:02:46 +0200

    wip

---
+++ src/Command/TipsterReport.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\PredictionRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'tipster:report')]
+class TipsterReport extends Command
+{
+    public function __construct(
+        private readonly PredictionRepository $predictionRepository,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('tipsterId', InputArgument::REQUIRED);
+        $this->addArgument('pctThreshold', InputArgument::REQUIRED);
+    }
+
+    public function __invoke(InputInterface $input, OutputInterface $output): int
+    {
+        $tipsterId = $input->getArgument('tipsterId');
+        $pctThreshold = $input->getArgument('pctThreshold');
+
+        $tipsterSummary = $this->predictionRepository->predictionsSummaryByTipster($tipsterId, $pctThreshold);
+
+        $homePredictionsPct = $tipsterSummary['totalHomePredictions'] === 0
+            ? 0
+            : floor(($tipsterSummary['totalHomePredictionsPositive'] * 100) / $tipsterSummary['totalHomePredictions']);
+
+        $visitorPredictionsPct = $tipsterSummary['totalVisitorPredictions'] === 0
+            ? 0
+            :floor(($tipsterSummary['totalVisitorPredictionsPositive'] * 100) / $tipsterSummary['totalVisitorPredictions']);
+
+        echo "Tipster: {$tipsterSummary['tipsterName']} \n";
+        echo "Pct threshold: {$pctThreshold} \n";
+        echo "Events: {$tipsterSummary['totalEvents']} \n";
+        echo "Home predictions: {$tipsterSummary['totalHomePredictions']} \n";
+        echo "Home wins: {$tipsterSummary['totalHomePredictionsPositive']} ({$homePredictionsPct}%) \n";
+        echo "Visitor predictions: {$tipsterSummary['totalVisitorPredictions']} \n";
+        echo "Visitor wins: {$tipsterSummary['totalVisitorPredictionsPositive']} ({$visitorPredictionsPct}%) \n";
+
+        return Command::SUCCESS;
+    }
+}
+
--- src/Repository/PredictionRepository.php
+++ src/Repository/PredictionRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Prediction;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\Persistence\ManagerRegistry;
+use Exception;
 
 class PredictionRepository extends ServiceEntityRepository
 {
@@ -45,5 +46,101 @@ class PredictionRepository extends ServiceEntityRepository
         }
 
         return $prediction;
+    }
+
+    public function predictionsSummaryByTipster(int $tipsterId, int $pctThreshold): array
+    {
+        $summary = [];
+        $tipsterName = '';
+        $totalEvents = 0;
+        $totalHomePredictions = 0;
+        $totalHomePredictionsPositive = 0;
+        $totalDrawPredictions = 0;
+        $totalDrawPredictionsPositive = 0;
+        $totalVisitorPredictions = 0;
+        $totalVisitorPredictionsPositive = 0;
+
+        $predictions = $this->getPredictionsWithEventData($tipsterId, $pctThreshold);
+
+        foreach ($predictions as $prediction) {
+            $tipsterName = $prediction['tipsterName'];
+
+            if ($prediction['eventHomeGoals'] === null || $prediction['eventVisitorGoals'] === null) {
+                continue;
+            }
+
+            $totalEvents++;
+
+            $predictionTeam = $this->getPredictionTeam(
+                $prediction['home_pct'],
+                $prediction['draw_pct'],
+                $prediction['visitor_pct']
+            );
+
+            if ($predictionTeam === "1") {
+                $totalHomePredictions++;
+
+                if ($prediction['eventHomeGoals'] > $prediction['eventVisitorGoals']) {
+                    $totalHomePredictionsPositive++;
+                }
+            } elseif ($predictionTeam === "X") {
+                $totalDrawPredictions++;
+
+                if ($prediction['eventHomeGoals'] === $prediction['eventVisitorGoals']) {
+                    $totalDrawPredictionsPositive++;
+                }
+            } elseif ($predictionTeam === "2") {
+                $totalVisitorPredictions++;
+
+                if ($prediction['eventHomeGoals'] < $prediction['eventVisitorGoals']) {
+                    $totalVisitorPredictionsPositive++;
+                }
+            }
+        }
+
+        $summary['tipsterName'] = $tipsterName;
+        $summary['totalEvents'] = $totalEvents;
+        $summary['totalHomePredictions'] = $totalHomePredictions;
+        $summary['totalHomePredictionsPositive'] = $totalHomePredictionsPositive;
+        $summary['totalDrawPredictions'] = $totalDrawPredictions;
+        $summary['totalDrawPredictionsPositive'] = $totalDrawPredictionsPositive;
+        $summary['totalVisitorPredictions'] = $totalVisitorPredictions;
+        $summary['totalVisitorPredictionsPositive'] = $totalVisitorPredictionsPositive;
+
+        if (($totalEvents - $totalHomePredictions - $totalDrawPredictions - $totalVisitorPredictions) !== 0) {
+            throw new Exception('Invalid number of events');
+        }
+
+        return $summary;
+    }
+
+    private function getPredictionsWithEventData(int $tipsterId, int $pctThreshold): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "
+            SELECT t.name AS tipsterName, e.home_goals AS eventHomeGoals, e.visitor_goals AS eventVisitorGoals, p.* 
+            FROM prediction p
+            JOIN event e ON e.id = p.event_id
+            JOIN tipster t ON t.id = p.tipster_id
+            WHERE p.tipster_id = :tipsterId AND (p.home_pct >= $pctThreshold || p.visitor_pct >= $pctThreshold)
+            ";
+
+        $resultSet = $conn->executeQuery($sql, ['tipsterId' => $tipsterId]);
+
+        return $resultSet->fetchAllAssociative();
+    }
+
+    private function getPredictionTeam(float $homePct, float $drawPct, float $visitorPct): string
+    {
+        $highestPct = max($homePct, $drawPct, $visitorPct);
+
+        if ($homePct === $highestPct) {
+            return "1";
+        } elseif ($drawPct === $highestPct) {
+            return "X";
+        } else {
+            return "2";
+        }
     }
 }
