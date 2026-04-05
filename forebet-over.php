c6db3e31a43cc520b36889d70d0dc1a1279f43a1
jordi <jordi.donadeu@nl.team.blue>
2026-04-05 18:41:16 +0200

    wip

---
+++ forebet-over.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'utils.php';
+include 'lib/ForeBet.php';
+
+$foreBet = new Forebet();
+
+$foreBetMatchesUnderOver = $foreBet->getUnderOverMatches();
+saveCsvFile('csv/forebet-under-over.csv', $foreBetMatchesUnderOver);
+
+echo "\n\n";
+echo "****************************************************** \n";
+echo "* +2,5 \n";
+echo "****************************************************** \n";
+
+foreach ($foreBetMatchesUnderOver as $match) {
+    if ($match['overPct'] < 90) {
+        continue;
+    }
+
+    echo "-- " . implode(",", $match) . "\n";
+}
